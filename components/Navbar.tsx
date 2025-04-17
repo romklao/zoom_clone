fa20145c6933f566ff31fbadbc450f19e6202020
Romklao Chainuwong <rk120879@gmail.com>
2025-04-17 15:11:55 -0700

    Add after sign out url

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -18,12 +18,12 @@ const Navbar = () => {
           className="max-sm:size-10"
         />
         <p className="text-[26px] font-extrabold text-white max-sm:hidden">
-          Boom
+          Yoom
         </p>
       </Link>
       <div className="flex-between gap-5">
         <SignedIn>
-          <UserButton />
+          <UserButton afterSignOutUrl="/sign-in" />
         </SignedIn>
         <MobileNav />
       </div>
